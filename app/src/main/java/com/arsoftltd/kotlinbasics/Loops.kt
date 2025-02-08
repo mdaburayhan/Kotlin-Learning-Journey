ae76d90c0c66b7b0032e4d85bf6307b35f45b3d4
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-02-09 01:09:36 +0600

    Updated project with loop

--- app/src/main/java/com/arsoftltd/kotlinbasics/Loops.kt
+++ app/src/main/java/com/arsoftltd/kotlinbasics/Loops.kt
@@ -4,9 +4,26 @@ fun main(){
     // Loops: used to repeat a block of code multiple times
 
     // for Loop: defined starting and ending points
+    for (i in 1..5){
+        println(i)
+    }
 
-    // while Loop:
-    // do-while Loop:
+    // while Loop: repeats a block of code as long as a specified condition is 'true'
+    var count = 1;
+    while (count<=5){
+        println("Count: $count")
+        count++
+    }
+
+    // do-while Loop: it guarantees that the loop body is
+    // executed at least once because the condition is checked
+    // after the loop body
+
+    var x = 6
+    do {
+        println("This will be printed at least once..")
+        x++
+    }while (x < 5)
 
 
 }
